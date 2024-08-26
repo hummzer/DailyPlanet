d2d1fb7fcd3e065e7ae7c0a35418c8033d95aed7
hummzer <zaeh888@gmail.com>
2024-08-26 09:51:15 +0300

    Form for submitting comments and its logic with redirects

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # We'll need 3 view functions: Index, Detail and Category
 
+from django.http import HttpResponseRedirect
+from main.forms import CommentForm
 from django.shortcuts import render
 from main.models import Post, Comment
 
@@ -30,9 +32,22 @@ def category(request, category):
 def detail(request, pk):
     post = Post.objects.get(pk=pk)
     comments = Comment.objects.filter(post=post)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+    comments = Comment.objects.filter(post=post)
     context = {
-            "post": post,
-            "comments": comments,
+        "post": post,
+        "comments": comments,
+        "form": CommentForm(),
     }
 
     return render(request, "main/detail.html", context)
